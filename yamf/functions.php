7c40c847feb196d7660339ca5f4fdda30631e75a
Michael Babienco <mbabienco@southern.edu>
2018-10-09 09:31:40 +0700

    Make routing with subfolders for controllers a bit easier on the user

--- yamf/functions.php
+++ yamf/functions.php
@@ -173,6 +173,10 @@
                     $output->controller = '\Controllers\\' . $path[1];
                     $output->function = $path[2];
                 }
+                // since we use PSR-4 and need \ for the "path" to controllers,
+                // we make it easy on users and let them use Parent-Folder/Controller-Name
+                // OR Parent-Folder\\Controller-Name in routes.php.
+                $output->controller = str_replace('/', '\\', $output->controller);
                 $output->routeParams = $foundParams;
                 $output->get = $getParams;
                 $output->anchor = $anchorOnPage;
